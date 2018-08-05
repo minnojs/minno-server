871d93387b5a4d67d2fc9b86629740b92408570f
ronenno1 <ronen.no1@gmail.com>
2018-08-05 17:16:43 +0300

    feat(users): add support with users managment

---
+++ routes/users_router.js
@@ -0,0 +1,35 @@
+const express     = require('express');
+const users        = require('../users');
+
+const usersRouter = express.Router();
+
+module.exports = usersRouter;
+
+usersRouter
+    .use(function is_logged_in(req, res, next){
+        if (!req.session || !req.session.user || req.session.user.role!=='su') return res.status(403).json({message: 'ERROR: Permission denied!'});
+        req.user_id = req.session.user.id;
+        next();
+    });
+
+usersRouter.route('')
+    .get(function(req, res){
+        return users.get_users()
+            .then(user_data=>res.json({users:user_data.filter(user=>user.id!==req.user_id)}))
+            .catch(err=>
+                res.status(err.status || 500).json({message:err.message}));
+    })
+    .delete(function(req, res){
+        return users.remove_user(req.body.user_id)
+            .then(user_data=>res.json({users:user_data}))
+            .catch(err=>
+                res.status(err.status || 500).json({message:err.message}));
+    })
+    .put(function(req, res){
+        return users.update_role(req.body.user_id, req.body.role)
+            .then(user_data=>res.json({users:user_data}))
+            .catch(err=>
+                res.status(err.status || 500).json({message:err.message}));
+    });
+
+
