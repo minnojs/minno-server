a21b3535efbcca628e17fddb75836a5c83619d11
eladzlot <ez.implicit@gmail.com>
2018-10-31 17:12:28 +0200

    feat(bank): added descriptions to all bank studies

--- bank/studyList.js
+++ bank/studyList.js
@@ -1,9 +1,9 @@
 module.exports =  [
-    {name:'race.demo', description: 'The classic Project implicit race IAT study', study_type: 'minno02'},
-    {name:'weight.demo', description: 'An IAT for evaluatign implicit weight', study_type: 'minno02'},
-    {name:'arab.demo', description: 'An IAT for evaluatign implicit arab', study_type: 'minno02'},
-    {name:'age.demo', description: 'An IAT for evaluatign implicit age', study_type: 'minno02'},
-    {name:'skin.demo', description: 'An IAT for evaluatign implicit skin', study_type: 'minno02'},
-    {name:'sexuality.demo', description: 'An IAT for evaluatign implicit sexuality', study_type: 'minno02'},
-    {name:'jspsych', description: 'how to run a jspsych study', study_type: 'html'}
+    {name:'race.demo', description: 'In a randomized order, participants complete a demographics questionnaire, a Race IAT (Black/White, Good/Bad) and a measure of explicit racial attitudes. Participants not on touch screen also complete the Modern Racism and Right-Wing Authoritarianism scales.', study_type: 'minno02'},
+    {name:'sexuality.demo', description: 'In a randomized order, participants complete a Sexuality IAT (Gay people / Straight people, Good/Bad) and a measure of explicit sexuality attitudes', study_type: 'minno02'},
+    {name:'age.demo', description: ' a randomized order, participants complete a Age IAT (Old people / Young people, Good/Bad) and a measure of explicit age attitudes', study_type: 'minno02'},
+    {name:'weight.demo', description: 'In a randomized order, participants complete a Weight IAT (Fat people / Thin people, Good/Bad) and a measure of explicit weight attitudes', study_type: 'minno02'},
+    {name:'skin.demo', description: 'In a randomized order, participants complete a Skin Tone IAT (Dark Skinned people / Light Skinned people, Good/Bad) and a measure of explicit skin tone attitudes.', study_type: 'minno02'},
+    {name:'arab.demo', description: 'In a randomized order, participants complete an Arab Muslims IAT (Arab Muslims / Other people, Good/Bad) and a measure of explicit Arab Muslim attitudes', study_type: 'minno02'},
+    {name:'jspsych', description: 'How to run a jspsych study', study_type: 'html'}
 ];
