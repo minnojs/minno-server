ca7d816583ae81b01ea088c83dfdc9414ecd28e8
Andrew Dzikiewicz <andy.dzik@gmail.com>
2019-07-22 22:50:35 -0400

    updates to support config file when server settings not saved in DB

--- server/server.js
+++ server/server.js
@@ -4,9 +4,13 @@ const Greenlock = require('greenlock-express');
 const config = require('../config');
 const configDb = require('./config_db');
 
-<<<<<<< HEAD
+
 exports.startupGreenlock = function(app, greenlock_data) {
     exports.shutdownHttps();
+	if(greenlock_data==null)
+	{
+		greenlock_data={owner_email: config.owner_email, domains:config.domains}
+	}
     const greenlock = Greenlock.create({
         // Let's Encrypt v2 is ACME draft 11
         version: 'draft-11',
@@ -27,45 +31,14 @@ exports.startupGreenlock = function(app, greenlock_data) {
         communityMember: true,
         debug: true
     });
-    const redirectHttps = require('redirect-https')();
+   /* const redirectHttps = require('redirect-https')();
     const acmeChallengeHandler = greenlock.middleware(redirectHttps);
     httpServer = require('http')
         .createServer(acmeChallengeHandler);
     httpServer.listen(config.port, function() {
         console.log('Listening for ACME http-01 challenges on', this.address());
-    });
-=======
-exports.startupGreenlock = function(app) {
-	exports.shutdownHttps();
-	const greenlock = Greenlock.create({
-		// Let's Encrypt v2 is ACME draft 11
-		version: 'draft-11',
-		server: 'https://acme-v02.api.letsencrypt.org/directory',
-		// Note: If at first you don't succeed, stop and switch to staging
-		//server: "https://acme-staging-v02.api.letsencrypt.org/directory",
-		// You MUST change this to a valid email address
-		email: config.owner_email,
-		// You MUST NOT build clients that accept the ToS without asking the user
-		agreeTos: true,
-		// You MUST change these to valid domains
-		// NOTE: all domains will validated and listed on the certificate
-		approvedDomains: config.domains,
-		// You MUST have access to write to directory where certs are saved
-		// ex: /home/foouser/acme/etc
-		configDir: '~/.config/acme/', // MUST have write access
-		// Get notified of important updates and help me make greenlock better
-		communityMember: true,
-		debug: true
-	});
-	/*const redirectHttps = require('redirect-https')();
-	const acmeChallengeHandler = greenlock.middleware(redirectHttps);
-	const acmeChallengeHandler = greenlock.middleware(redirectHttps);
-	httpServer = require('http')
-		.createServer(acmeChallengeHandler);
-	httpServer.listen(config.port, function() {
-		console.log('Listening for ACME http-01 challenges on', this.address());
-	});*/
->>>>>>> greenlock updates
+    });*/
+
 
     ////////////////////////
     // http2 via SPDY h2  //
@@ -106,47 +79,27 @@ exports.startupHttps = function(app, server_data) {
     const fs = require('fs');
     const http = require('http');
     const https = require('https');
-    const privateKey = server_data.private_key;
-    const certificate = server_data.certificate;
+if(server_data==null)
+{
+	server_data={privateKey:config.keyFile,certificate:config.certFile,port:config.sslport}
+}
 
-<<<<<<< HEAD
     const credentials = {
-        key: privateKey,
-        cert: certificate
+        key: server_data.privateKey,
+        cert: server_data.certificate
     };
-    httpServer = http.createServer(app);
-    httpServer.listen(config.port);
 
     try{
         httpsServer = https.createServer(credentials, app);
         httpsServer.listen(server_data.port);
-        console.log('Minno-server Started on PORT ' + config.port + ' and ' + server_data.port);
+        console.log('Minno-server Started on PORT ' + server_data.port);
     }
     catch(e){
 		console.log(e);
-        return this.startupHttp(app);
+        return true;
     }
 };
 
-exports.shutdownHttps = function() {
-    if (httpServer != null) {
-        httpServer.close();
-        httpServer = null;
-    }
-    if (httpsServer != null) {
-        httpsServer.close();
-        httpsServer = null;
-    }
-};
-=======
-	const credentials = {
-		key: privateKey,
-		cert: certificate
-	};
-	httpsServer = https.createServer(credentials, app);
-	httpsServer.listen(config.sslport);
-	console.log('Minno-server Started on PORT ' + config.port + 'and ' + config.sslport);
-}
 
 exports.shutdownHttps = function() {
 	if (httpsServer != null) {
@@ -154,4 +107,3 @@ exports.shutdownHttps = function() {
 		httpsServer = null;
 	}
 }
->>>>>>> greenlock updates
