1d9b8745410272e85efa7614cc3616a0be5a6a11
ronenno1 <ronenno1@gmail.com>
2021-02-08 19:35:34 +0200

    fix(versions): try new style

--- dashboard/src/study/properties/propertiesComponent.js
+++ dashboard/src/study/properties/propertiesComponent.js
@@ -276,23 +276,22 @@ let propertiesComponent = {
                         m('.col-sm-12.space',  m('h4', 'Versions'))
                     ),
                     m('.row.space',
-                        m('.col-sm-6.space',
+                        m('.col-sm-7.space',
+                            m('table.table',
+                                ctrl.study.versions.map((version, id)=>
+                                    m('tr', [
+                                        m('td',  [m('strong', ['v', version.id]), ` (${formatDate(version.creation_date)})`]),
+                                        m('td', m('button.btn.btn-primary.btn-sm', {onclick: function(){m.route(`/editor/${ctrl.study.id}/${ctrl.study.versions.length===id+1 ? '': version.id}`);}}, version.state==='Develop' && !ctrl.study.isReadonly ? 'Edit' : 'Review')),
+                                        ctrl.study.isReadonly ? ''     :
+                                            m('td', m('button.btn.btn-primary.btn-sm', {onclick: ()=>ctrl.show_change_availability(ctrl.study, version.hash, !version.availability)}, version.availability ? 'Active' : 'Inactive')),
+                                                version.state!=='Develop' ? '' :
+                                                    m('td', m('button.btn.btn-primary.btn-sm', {onclick:ctrl.show_publish}, 'Publish'))
 
-                    m('table.table',
-                        ctrl.study.versions.map((version, id)=>
-                            m('tr', [
-                                m('td',  [m('strong', ['v', version.id]), ` (${formatDate(version.creation_date)})`]),
-                                m('td', m('button.btn.btn-primary.btn-sm', {onclick: function(){m.route(`/editor/${ctrl.study.id}/${ctrl.study.versions.length===id+1 ? '': version.id}`);}}, version.state==='Develop' && !ctrl.study.isReadonly ? 'Edit' : 'Review')),
-
-
-                                ctrl.study.isReadonly ? ''     :
-                                    m('td', m('button.btn.btn-primary.btn-sm', {onclick: ()=>ctrl.show_change_availability(ctrl.study, version.hash, !version.availability)}, version.availability ? 'Active' : 'Inactive')),
-                                        version.state!=='Develop' ? '' :
-                                            m('td', m('button.btn.btn-primary.btn-sm', {onclick:ctrl.show_publish}, 'Publish'))
-
-                            ])
+                                    ])
+                                )
+                            )
                         )
-                    )))
+                    )
                 ],
 
                 m('.row.space',
