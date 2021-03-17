ff05acdb1cf1ba54577e99194bce053f0c8ce75a
Andrew Dzikiewicz <andy.dzik@gmail.com>
2021-03-17 09:17:40 -0400

    research pool updates

---
+++ server/data_server/controllers/demographicsController.js
@@ -0,0 +1,70 @@
+'use strict';
+const mongoose = require('mongoose'),
+    Data2 = require('../models/demographicsSchema'),
+    Demographics = mongoose.model('Demographics'),
+    logger = require('../../logger');
+
+
+
+exports.insertDemographics = async function(req,res) {
+    let reqBody = req.body;
+    reqBody = sanitizeMongoJson(reqBody);
+	console.log(reqBody);
+    let newData = new Demographics(reqBody);
+
+    await newData.save(function(err, data) {
+        if (err) {
+            console.log(err);
+            logger.error({
+                message: err
+            });
+        }
+        res.status(200).json({result:'success'});
+    });
+};
+
+
+
+
+exports.getUserDemographics = async function(registrationId) {
+
+    let results = await Demographics.find({
+        registration_Id: registrationId
+    }, (err, dataRequests) => {
+        if (err) {
+            throw err;
+        } else {
+            if (dataRequests) {
+                return dataRequests;
+            } else {
+                return {};
+            }
+        }
+
+
+    });
+    return results;
+};
+let sanitizeMongoJson = function(mongoJson) {
+    if (Array.isArray(mongoJson)) {
+        mongoJson.forEach(element => sanitizeMongoJson(element));
+    } else {
+        if (mongoJson instanceof Object) {
+            mongoJson = sanitizeMongo(mongoJson);
+            for (let key in mongoJson) {
+                mongoJson[key] = sanitizeMongoJson(mongoJson[key]);
+            }
+        }
+    }
+    return mongoJson;
+};
+let sanitizeMongo = function(v) {
+    if (v instanceof Object) {
+        for (let key in v) {
+            if (/^\$/.test(key)) {
+                delete v[key];
+            }
+        }
+    }
+    return v;
+};
