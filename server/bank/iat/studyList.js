421cfa63615a9294cc754978193ac7802727a0c0
ronenno1 <ronenno1@gmail.com>
2021-06-17 13:05:27 +0300

    fix(player): move to v0.3

--- server/bank/iat/studyList.js
+++ server/bank/iat/studyList.js
@@ -1,12 +1,12 @@
 /* eslint-env node */
 module.exports =  [
-    {name:'irace.demo', description: 'In a randomized order, participants complete a demographics questionnaire, a Race IAT (Black/White, Good/Bad) and a measure of explicit racial attitudes. Participants not on touch screen also complete the Modern Racism and Right-Wing Authoritarianism scales.', study_type: 'minno02'},
-    {name:'sexuality.demo', description: 'In a randomized order, participants complete a Sexuality IAT (Gay people / Straight people, Good/Bad) and a measure of explicit sexuality attitudes', study_type: 'minno02'},
-    {name:'age.demo', description: ' a randomized order, participants complete a Age IAT (Old people / Young people, Good/Bad) and a measure of explicit age attitudes', study_type: 'minno02'},
-    {name:'weight.demo', description: 'In a randomized order, participants complete a Weight IAT (Fat people / Thin people, Good/Bad) and a measure of explicit weight attitudes', study_type: 'minno02'},
-    {name:'skin.demo', description: 'In a randomized order, participants complete a Skin Tone IAT (Dark Skinned people / Light Skinned people, Good/Bad) and a measure of explicit skin tone attitudes.', study_type: 'minno02'},
-    {name:'arab.demo', description: 'In a randomized order, participants complete an Arab Muslims IAT (Arab Muslims / Other people, Good/Bad) and a measure of explicit Arab Muslim attitudes', study_type: 'minno02'},
-    {name:'time.examples', description: 'A collection of advanced minno-time tasks', study_type: 'minno02'},
-    {name:'heb.iat.example', description: 'This is an example study in Hebrew. It includes an IAT, IAT instructions page, a questionnaire, and debriefing that shows the results of the IAT. When testing the IAT, if you want to skip blocks, press ESC and then Enter.', study_type: 'minno02'},
+    {name:'irace.demo', description: 'In a randomized order, participants complete a demographics questionnaire, a Race IAT (Black/White, Good/Bad) and a measure of explicit racial attitudes. Participants not on touch screen also complete the Modern Racism and Right-Wing Authoritarianism scales.', study_type: 'minno03'},
+    {name:'sexuality.demo', description: 'In a randomized order, participants complete a Sexuality IAT (Gay people / Straight people, Good/Bad) and a measure of explicit sexuality attitudes', study_type: 'minno03'},
+    {name:'age.demo', description: ' a randomized order, participants complete a Age IAT (Old people / Young people, Good/Bad) and a measure of explicit age attitudes', study_type: 'minno03'},
+    {name:'weight.demo', description: 'In a randomized order, participants complete a Weight IAT (Fat people / Thin people, Good/Bad) and a measure of explicit weight attitudes', study_type: 'minno03'},
+    {name:'skin.demo', description: 'In a randomized order, participants complete a Skin Tone IAT (Dark Skinned people / Light Skinned people, Good/Bad) and a measure of explicit skin tone attitudes.', study_type: 'minno03'},
+    {name:'arab.demo', description: 'In a randomized order, participants complete an Arab Muslims IAT (Arab Muslims / Other people, Good/Bad) and a measure of explicit Arab Muslim attitudes', study_type: 'minno03'},
+    {name:'time.examples', description: 'A collection of advanced minno-time tasks', study_type: 'minno03'},
+    {name:'heb.iat.example', description: 'This is an example study in Hebrew. It includes an IAT, IAT instructions page, a questionnaire, and debriefing that shows the results of the IAT. When testing the IAT, if you want to skip blocks, press ESC and then Enter.', study_type: 'minno03'},
     {name:'jspsych', description: 'How to run a jspsych study', study_type: 'html'}
 ];
