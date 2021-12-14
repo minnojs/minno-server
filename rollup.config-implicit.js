e17df6d22dbd2abd425105a9de9ed953a0f54e1b
elinorbengayev <elinorb240@gmail.com>
2021-12-14 17:03:40 +0200

    updated implicit measures

---
+++ rollup.config-implicit.js
@@ -0,0 +1,27 @@
+import {version} from './package.json';
+
+function createBanner(type){
+    return `/**
+    * @preserve minnojs-${type}-dashboard v${version}
+    * @license Apache-2.0 (${(new Date()).getFullYear()})
+    */
+    `;
+}
+
+function configTask(type){
+    return {
+        input: `./dashboard/src/study/files/wizards/implicitMeasures/${type.toUpperCase()}/${type}.index.standalone.js`,
+        output: {
+            file: `./docs/implicitMeasures/jsFiles/${type}_index.js`,
+            format: 'iife',
+            name: `${type}Dashboard`,
+            sourcemap:true,
+            banner: createBanner(type)
+        }
+    };
+}
+
+
+
+export default [configTask('iat'), configTask('biat'), configTask('stiat'),
+    configTask('spf'), configTask('ep')];
