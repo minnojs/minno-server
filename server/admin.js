333712c13c5db9354ad3f3ae0602546bc8acc55d
ronenno1 <ronenno1@gmail.com>
2022-07-24 10:28:59 +0300

    fix(data usage): remove duplications

--- server/admin.js
+++ server/admin.js
@@ -8,16 +8,18 @@ const connection    = Promise.resolve(require('mongoose').connection);
 
 exports.get_data_usage = function () {
     return connection.then(function (db) {
-        const studies = db.collection('studies');
         const data_usage = db.collection('data_usage');
         const users = db.collection('users');
         return data_usage.find({}).toArray().then(all_data_usage=>{
-            return users.find({}).toArray().then(users=>users.flatMap(user=>{
-                const study_ids = user.studies.filter(study=>!study.permission).flatMap(study=>study.id);
-                return ({user_id: user._id, user_name: user.user_name, studies_per_day: all_data_usage.flatMap(daily_data_usage=>
-                    ({date: daily_data_usage.date, studies: daily_data_usage.studies.filter(study=>study_ids.includes(study.id))})
-                )});
-            }));
+            return users.find({}).toArray().then(users=>Promise.all(users.map(user=>{
+                return studies_comp.get_studies(user._id)
+                    .then(studies=>{
+                        const study_ids = studies.filter(study=>study.permission ==='owner')
+                        .map(user_study=>user_study.id);
+                        return ({user_id: user._id, user_name: user.user_name, studies_per_day: all_data_usage
+                                .flatMap(daily_data_usage=> ({date: daily_data_usage.date, studies: daily_data_usage.studies.filter(study=>study_ids.includes(study.id))}))});
+                    });
+            })));
         });
     });
 };
@@ -26,7 +28,7 @@ exports.get_data_usage = function () {
 exports.get_usage = function () {
 
     return this.get_data_usage().then(data_usage=> {
-        const usae_data_arr = execSync('df -hT /home').toString().split('\n')[1].replace(/ +/g, '_').split('_');
+        const usage_data_arr = execSync('df -hT /home').toString().split('\n')[1].replace(/ +/g, '_').split('_');
         return fs.readdir(config_file.user_folder)
             .then(users =>
                 Promise.all(users.map(function (user) {
@@ -51,13 +53,13 @@ exports.get_usage = function () {
                 return ({
                     storage_per_user,
                     data_usage,
-                    Filesystem: usae_data_arr[0],
-                    Type: usae_data_arr[1],
-                    Size: usae_data_arr[2],
-                    Used: usae_data_arr[3],
-                    Avail: usae_data_arr[4],
-                    Use: usae_data_arr[5],
-                    MountedOn: usae_data_arr[6]
+                    Filesystem: usage_data_arr[0],
+                    Type: usage_data_arr[1],
+                    Size: usage_data_arr[2],
+                    Used: usage_data_arr[3],
+                    Avail: usage_data_arr[4],
+                    Use: usage_data_arr[5],
+                    MountedOn: usage_data_arr[6]
                 });
             });
     });
