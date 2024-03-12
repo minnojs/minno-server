4844bfc1be2d524b9e7b0d69da7572fc72e8d290
ronenno1 <ronenno1@gmail.com>
2024-03-12 11:32:32 +0200

    feat(dp): sorting

--- dashboard/dist/main.js
+++ dashboard/dist/main.js
@@ -25632,6 +25632,9 @@
                 tokens:m.prop(),
                 loaded:false,
                 col_error:m.prop(''),
+                order_by:m.prop(''),
+                sort_studies_by: sort_studies_by,
+
                 remove: remove};
 
             function remove(token_id){
@@ -25651,12 +25654,96 @@
                 get_tokens()
                     .then(function (response) { return ctrl.tokens(response.tokens); })
                     .then(function (){ return ctrl.loaded = true; })
+                    .then(function (){ return console.log(ctrl.tokens()); })
+
                     .catch(function (error) {
                         ctrl.col_error(error.message);
                     }).then(m.redraw);
             }
             load();
             return ctrl;
+            function sort_studies_by_uses(study1, study2){
+                ctrl.order_by('uses');
+                return study1.uses === study2.uses ? 0 : study1.uses < study2.uses ? 1 : -1;
+            }
+
+            function sort_studies_by_uses_dec(study1, study2){
+                ctrl.order_by('uses_dec');
+                return study1.uses === study2.uses ? 0 : study1.uses > study2.uses ? 1 : -1;
+            }
+
+
+            function sort_studies_by_domain(study1, study2){
+                study1.domain = study1.domain ? study1.domain : '';
+                study2.domain = study2.domain ? study2.domain : '';
+                ctrl.order_by('domain');
+                return study1.domain.toLowerCase() === study2.domain.toLowerCase() ? 0 : study1.domain.toLowerCase() < study2.domain.toLowerCase() ? -1 : 1;
+            }
+
+            function sort_studies_by_domain_dec(study1, study2){
+                ctrl.order_by('domain_dec');
+                return study1.domain.toLowerCase() === study2.domain.toLowerCase() ? 0 : study1.domain.toLowerCase() < study2.domain.toLowerCase() ? 1 : -1;
+            }
+
+            function sort_studies_by_first(study1, study2){
+                ctrl.order_by('first');
+                return study1.first_use === study2.first_use ? 0 : study1.first_use < study2.first_use ? -1 : 1;
+            }
+
+            function sort_studies_by_first_dec(study1, study2){
+                ctrl.order_by('first_dec');
+                return study1.first_use === study2.first_use ? 0 : study1.first_use > study2.first_use ? -1 : 1;
+            }
+
+            function sort_studies_by_last(study1, study2){
+                ctrl.order_by('last');
+                return study1.last_use === study2.last_use ? 0 : study1.last_use < study2.last_use ? -1 : 1;
+            }
+
+            function sort_studies_by_last_dec(study1, study2){
+                ctrl.order_by('last_dec');
+                return study1.last_use === study2.last_use ? 0 : study1.last_use > study2.last_use ? -1 : 1;
+            }
+
+            function sort_studies_by_debug(study1, study2){
+                ctrl.order_by('debug');
+                study1.debug = study1.debug ? 1 : 0;
+                study2.debug = study2.debug ? 1 : 0;
+                return study1.debug === study2.debug ? 0 : study1.debug < study2.debug ? -1 : 1;
+            }
+
+            function sort_studies_by_debug_dec(study1, study2){
+                ctrl.order_by('debug_dec');
+                return study1.debug === study2.debug ? 0 : study1.debug > study2.debug ? -1 : 1;
+            }
+
+            function sort_studies_by(name){
+                if (name==='uses') {
+                    if (ctrl.order_by() === 'uses')
+                        { return ctrl.tokens(ctrl.tokens().sort(sort_studies_by_uses_dec)); }
+                    return ctrl.tokens(ctrl.tokens().sort(sort_studies_by_uses));
+                }
+                if (name==='domain') {
+                    if (ctrl.order_by() === 'domain')
+                        { return ctrl.tokens(ctrl.tokens().sort(sort_studies_by_domain_dec)); }
+                    return ctrl.tokens(ctrl.tokens().sort(sort_studies_by_domain));
+                }
+                if (name==='first') {
+                    if (ctrl.order_by() === 'first')
+                        { return ctrl.tokens(ctrl.tokens().sort(sort_studies_by_first_dec)); }
+                    return ctrl.tokens(ctrl.tokens().sort(sort_studies_by_first));
+                }
+                if (name==='last') {
+                    if (ctrl.order_by() === 'last')
+                        { return ctrl.tokens(ctrl.tokens().sort(sort_studies_by_last_dec)); }
+                    return ctrl.tokens(ctrl.tokens().sort(sort_studies_by_last));
+                }
+                if (name==='debug') {
+                    if (ctrl.order_by() === 'debug')
+                        { return ctrl.tokens(ctrl.tokens().sort(sort_studies_by_debug_dec)); }
+                    return ctrl.tokens(ctrl.tokens().sort(sort_studies_by_debug));
+                }
+            }
         },
         view: function view(ctrl){
             return  !ctrl.loaded
@@ -25672,14 +25759,50 @@
                     m('table', {class:'table table-striped table-hover'}, [
                         m('thead', [
                             m('tr', [
-                                m('th', 'Token'),
-                                m('th', 'Domain'),
-                                m('th',  'Uses'),
-                                m('th',  'First use'),
-                                m('th',  'Last use'),
-                                m('th',  'Debug'),
-                                m('th',  'Remove')
-                            ])
+                                m('th.',
+                                    m('.form-control-static',{onclick:function (){ return ctrl.sort_studies_by('domain'); }, style:'cursor:pointer'},[
+                                        m('i', ''),
+                                        m('p', m('strong', 'Token '))
+                                    ])
+                                ),
+                                m('th',
+                                    m('.form-control-static',{onclick:function (){ return ctrl.sort_studies_by('domain'); }, style:'cursor:pointer'},[
+                                        m('i.fa.fa-sort', {style: {color: ctrl.order_by()==='domain' ? 'black' : 'grey'}}),
+                                        m('p', m('strong', 'Domain '))
+                                    ])
+                                ),
+                                m('th.col-md-3',
+                                    m('.form-control-static',{onclick:function (){ return ctrl.sort_studies_by('uses'); }, style:'cursor:pointer'},[
+                                        m('i.fa.fa-sort', {style: {color: ctrl.order_by()==='uses' ? 'black' : 'grey'}}),
+                                        m('p', m('strong', 'Uses '))
+
+                                    ])
+                                ),
+                                m('th',
+                                    m('.form-control-static',{onclick:function (){ return ctrl.sort_studies_by('first'); }, style:'cursor:pointer'},[
+                                        m('i.fa.fa-sort', {style: {color: ctrl.order_by()==='first' ? 'black' : 'grey'}}),
+                                        m('p', m('strong', 'First use '))
+
+                                    ])
+                                ),
+                                m('th',
+                                    m('.form-control-static',{onclick:function (){ return ctrl.sort_studies_by('last'); }, style:'cursor:pointer'},[
+                                        m('i.fa.fa-sort', {style: {color: ctrl.order_by()==='last' ? 'black' : 'grey'}}),
+                                        m('p', m('strong', 'Last use '))
+
+                                    ])
+                                ),
+                                m('th',  m('.form-control-static',{onclick:function (){ return ctrl.sort_studies_by('debug'); }, style:'cursor:pointer'},[
+                                    m('i.fa.fa-sort', {style: {color: ctrl.order_by()==='debug' ? 'black' : 'grey'}}),
+                                    m('p', m('strong', 'Debug '))
+
+                                ])),
+                                m('th.',
+                                    m('.form-control-static',{onclick:function (){ return ctrl.sort_studies_by('domain'); }, style:'cursor:pointer'},[
+                                        m('i', ''),
+                                        m('p', m('strong', 'Remove '))
+                                    ])
+                                ) ])
                         ]),
                         m('tbody', [
                             ctrl.tokens().map(function (token) { return m('tr', [
